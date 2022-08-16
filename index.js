fc13b11b1acef0fc82e70a61a5e6e7d2e6ea4754
Mulu <brianmulu007@gmail.com>
2022-08-16 16:16:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') {
+    return (`This Saturday, I want to ${defaultActivity}!`)
+}
+
+function mondayWork (defaultActivty = 'go to the office') {
+    return (`This Monday, I will ${defaultActivty}.`)
+}
+
+const wrapAdjective = function(ngori="*") {
+    return function(adjective = 'special' ) {
+        return `You are ${ngori}${adjective}${ngori}!`
+    }
+}
